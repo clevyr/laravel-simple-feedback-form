7ca428f2dfe26ae384f09d61097b8065cb1a4fb1
Adam Libby <adam.libby@clevyr.com>
2022-06-09 13:29:55 -0500

    Extracted variables to props and cleanup

--- resources/js/FeedbackForm.vue
+++ resources/js/FeedbackForm.vue
@@ -1,260 +1,324 @@
 <template>
-    <section class="feedback-form">
-        <form
-            v-if="isActive"
-            v-closable="{
-              exclude: ['formToggle'],
-              handler: 'onClose'
-            }"
-            class="form mb-3"
-            role="form"
-            @submit.prevent="onSubmit"
-        >
-            <div class="form-group">
-                <label
-                    class="form-label"
-                    for="feedback-name"
-                >Name:*</label>
-                <input
-                    class="form-control"
-                    v-model="nameField"
-                    id="feedback-name"
-                    type="text"
-                />
-            </div>
-            <div class="form-group">
-                <label
-                    class="form-label"
-                    for="feedback-email"
-                >Email:</label>
-                <input
-                    class="form-control"
-                    v-model="emailField"
-                    id="feedback-email"
-                    type="email"
-                />
-            </div>
-            <div class="form-group">
-                <label
-                    class="form-label"
-                    for="feedback-body"
-                >Comment:*</label>
-                <textarea
-                    class="form-control"
-                    resize="false"
-                    id="feedback-body"
-                    type="text"
-                    v-model="body"
-                />
-            </div>
-            <p v-if="success" class="text-success text-center">
-                Your feedback<br />has been submitted.
-            </p>
-            <p v-if="errors" class="text-danger text-center">
-              {{ errors }}
-            </p>
-            <button
-                :disabled="!isValid"
-                class="form-control btn btn-primary"
-                type="submit"
-            >
-                <div
-                    v-if="isLoading"
-                    class="spinner-border spinner-border-sm"
-                    role="status"
-                >
-                    <span class="sr-only">Loading...</span>
-                </div>
-                <div v-else>
-                    Submit
-                </div>
-            </button>
-        </form>
-        <div class="text-right">
-            <button
-                ref="formToggle"
-                class="feedback-btn btn btn-primary"
-                @click="toggleForm"
-            >
-                <i class="fa fa-question" />
-            </button>
-        </div>
-    </section>
+	<div>
+		<button
+			ref="formToggle"
+			class="feedback-btn btn btn-primary"
+			@click="toggleForm"
+		>
+			<span>
+				{{ buttonLabel }}
+			</span>
+		</button>
+		<div
+			v-if="isActive"
+			class="feedback-form"
+			id="feedbackform"
+			role="dialog"
+			aria-modal="false"
+			ref="dialog"
+			aria-labelledby="feedbackform-title"
+			tabindex="-1"
+		>
+			<form
+				class="form"
+				@submit.prevent="onSubmit"
+			>
+				<h2 class="mb-2 h6 text-sm" id="feedbackform-title">
+					{{ formTitle }}
+				</h2>
+				<p v-if="success" class="text-success text-center whitespace-pre-wrap">
+					{{ successMessage }}
+				</p>
+				<p v-if="errors" class="text-danger text-center">
+				{{ errors }}
+				</p>
+				<div class="form-control">
+					<label
+						class="form-label"
+						for="feedback-name"
+					>
+						Name
+						<span
+							class="required"
+							aria-hidden="true"
+						>
+							*
+						</span>
+					</label>
+					<input
+						v-model="nameField"
+						id="feedback-name"
+						type="text"
+						ref="nameInputField"
+						required
+					/>
+				</div>
+				<div class="form-control">
+					<label
+						class="form-label"
+						for="feedback-email"
+					>Email</label>
+					<input
+						v-model="emailField"
+						id="feedback-email"
+						type="email"
+					/>
+				</div>
+				<div class="form-control">
+					<label
+						class="form-label"
+						for="feedback-body"
+					>
+						Comment
+						<span
+							class="required"
+							aria-hidden="true"
+						>
+							*
+						</span>
+					</label>
+					<textarea
+						class="resize-none"
+						id="feedback-body"
+						type="text"
+						v-model="comment"
+						required
+					/>
+				</div>
+				<button
+					:disabled="!isValid"
+					class="btn btn-primary"
+					type="submit"
+				>
+					<svg
+						v-if="isLoading"
+						class="loader"
+						viewBox="8 8 16 16"
+						fill="none"
+						width="32"
+						height="32"
+					>
+						<circle
+							cx="16"
+							cy="16"
+							r="7"
+							fill="none"
+							stroke="#000"
+							opacity=".1"
+							stroke-width="2"
+						/>
+						<circle
+							class="loader-path"
+							cx="16"
+							cy="16"
+							r="7"
+							fill="none"
+							stroke="#70c542"
+							stroke-width="2"
+						/>
+					</svg>
+					<div v-else>
+						{{ submitButtonLabel }}
+					</div>
+				</button>
+			</form>
+		</div>
+	</div>
 </template>
-
-<style lang="scss">
-.feedback-form {
-    position: fixed;
-    z-index: 100;
-    bottom: 1em;
-    right: 1em;
-
-    .form {
-        max-width: 16em;
-        padding: 1em;
-        background: white;
-        border-radius: 5px;
-        border: 1px solid darkgray;
-    }
-
-    .feedback-btn {
-        font-size: 1.5em;
-        border-radius: 50%;
-        width: 50px;
-        height: 50px;
-        color: white;
-    }
-}
-</style>
 
 <script>
 import axios from 'axios';
 
-// Closable Directive courtesy of
-// https://tahazsh.com/detect-outside-click-in-vue
-
-// This variable will hold the reference to
-// document's click handler
-let handleOutsideClick;
-
-const Closable = {
-    bind(el, binding, vnode) {
-        // Here's the click/touchstart handler
-        // (it is registered below)
-        handleOutsideClick = (e) => {
-            e.stopPropagation();
-            // Get the handler method name and the exclude array
-            // from the object used in v-closable
-            const { handler, exclude } = binding.value;
-
-            // This variable indicates if the clicked element is excluded
-            let clickedOnExcludedEl = false;
-            exclude.forEach((refName) => {
-                // We only run this code if we haven't detected
-                // any excluded element yet
-                if (!clickedOnExcludedEl) {
-                    // Get the element using the reference name
-                    const excludedEl = vnode.context.$refs[refName];
-                    // See if this excluded element
-                    // is the same element the user just clicked on
-                    clickedOnExcludedEl = excludedEl.contains(e.target);
-                }
-            });
-
-            // We check to see if the clicked element is not
-            // the dialog element and not excluded
-            if (!el.contains(e.target) && !clickedOnExcludedEl) {
-                // If the clicked element is outside the dialog
-                // and not the button, then call the outside-click handler
-                // from the same component this directive is used in
-                vnode.context[handler]();
-            }
-        };
-        // Register click/touchstart event listeners on the whole page
-        document.addEventListener('click', handleOutsideClick);
-        document.addEventListener('touchstart', handleOutsideClick);
-    },
-
-    unbind() {
-        // If the element that has v-closable is removed, then
-        // unbind click/touchstart listeners from the whole page
-        document.removeEventListener('click', handleOutsideClick);
-        document.removeEventListener('touchstart', handleOutsideClick);
-    },
-};
-
 export default {
-    props: ['name', 'email'],
+	props: {
+		name: {
+			type: String,
+			default: '',
+		},
+		email: {
+			type: String,
+			default: '',
+		},
+		buttonLabel: {
+			type: String,
+			default: '?',
+		},
+		formTitle: {
+			type: String,
+			default: '',
+		},
+		successMessage: {
+			type: String,
+			default: "Your feedback\nhas been submitted!"
+		},
+		submitButtonLabel: {
+			type: String,
+			default: "Submit",
+		}
+	},
 
-    data() {
-        return {
-            emailField: this.email,
-            nameField: this.name,
-            body: null,
-            isActive: false,
-            isLoading: false,
-            errors: null,
-            success: false,
-        };
-    },
+	data() {
+		return {
+			emailField: this.email ?? '',
+			nameField: this.name ?? '',
+			comment: null,
+			isActive: false,
+			isLoading: false,
+			errors: null,
+			success: false,
+		};
+	},
 
-    directives: {
-        Closable,
-    },
+	computed: {
+		isValid() {
+			if (this.isLoading) {
+				return false;
+			}
 
-    computed: {
-        isValid() {
-            console.log('in is valid');
-            if (this.isLoading) {
-                console.log('invalid 1');
-                return false;
-            }
+			if (!this.nameField) {
+				return false;
+			}
 
-            if (!this.name) {
-                console.log('invalid 2');
-                return false;
-            }
+			if (!this.comment) {
+				return false;
+			}
 
-            if (!this.body) {
-                console.log('invalid 3');
-                return false;
-            }
+			return true;
+		},
+	},
 
-            console.log('valid');
-            return true;
-        },
-    },
+	methods: {
+		toggleForm() {
+			this.isActive ? this.hideForm() : this.showForm();
+		},
 
-    methods: {
-        toggleForm() {
-            this.isActive = !this.isActive;
-        },
+		showForm() {
+			this.isActive = true;
 
-        onClose() {
-            this.isActive = false;
-        },
+			this.$nextTick(() => {
+				document.addEventListener('keydown', this.closeOnEscape);
+				document.addEventListener('click', this.closeOnClickOutside);
+				document.addEventListener('focusin', this.closeOnFocusOutside);
 
-        async onSubmit() {
-            try {
-                this.errors = null;
-                this.success = false;
-                this.isLoading = true,
+				this.$refs.nameInputField.focus();
+			});
+		},
 
-                await axios.post(`/simple-feedback`, {
-                    name: this.nameField,
-                    email: this.emailField,
-                    body: this.body,
-                });
+		hideForm() {
+			this.isActive = false;
 
-                this.isLoading = false;
-                this.success = true;
+			this.$nextTick(() => {
+				document.removeEventListener('keydown', this.closeOnEscape);
+				document.removeEventListener('click', this.closeOnClickOutside);
+				document.removeEventListener('focusin', this.closeOnFocusOutside);
 
-                this.reset();
-            } catch (error) {
-                this.isLoading = false;
-                this.errors = error.message;
-            }
-        },
+			});
+		},
 
-        reset() {
-            this.body = null;
-        },
-    },
+		closeOnEscape(e) {
+			if (e.keyCode === 27 && this.isActive) {
+				this.hideForm();
 
-    mounted() {
-        this.reset();
-    },
+				this.$nextTick(() => {
+					this.$refs.toggleForm.focus();
+				});
+			}
+		},
 
-    watch: {
-        isActive(value) {
-            if (value) {
-                return;
-            }
+		closeOnClickOutside(e) {
+			if (!this.$refs.dialog) {
+				return;
+			}
 
-            // Reset the state of the form.
-            this.success = false;
-            this.errors = null;
-        },
-    },
+			if (e.target === this.$refs.dialog ||
+				this.$refs.dialog.contains(e.target) ||
+				e.target === this.$refs.formToggle
+			) {
+				return;
+			}
+
+			this.hideForm();
+		},
+
+		closeOnFocusOutside() {
+			this.closeOnClickOutside({target: document.activeElement});
+		},
+
+		async onSubmit() {
+			try {
+				this.errors = null;
+				this.success = false;
+				this.isLoading = true,
+
+				await axios.post(`/simple-feedback`, {
+					name: this.nameField,
+					email: this.emailField,
+					comment: this.comment,
+				});
+
+				this.isLoading = false;
+				this.success = true;
+
+				this.reset();
+			} catch (error) {
+				this.isLoading = false;
+				this.errors = error.message;
+			}
+		},
+
+		reset() {
+			this.comment = null;
+		},
+	},
+
+	mounted() {
+		this.reset();
+	},
+
+	watch: {
+		isActive(value) {
+			if (value) {
+				return;
+			}
+
+			// Reset the state of the form.
+			this.success = false;
+			this.errors = null;
+		},
+	},
 };
 </script>
+
+<style scoped lang="scss">
+.feedback-form {
+	position: fixed;
+	z-index: 99;
+	bottom: 4.5em;
+	right: 1em;
+	max-width: 24em;
+
+	.form {
+		max-width: 16em;
+		padding: 2em;
+		margin-bottom: 1em;
+	}
+	.form-control {
+		width: 100%;
+		margin-bottom: 1em;
+		display: block;
+	}
+}
+.feedback-btn {
+	position: fixed;
+	z-index: 100;
+	bottom: 1em;
+	right: 1em;
+	font-size: 1em;
+	padding: .5em 1em;
+	text-align: center;
+
+	span {
+		pointer-events: none;
+	}
+}
+</style>
